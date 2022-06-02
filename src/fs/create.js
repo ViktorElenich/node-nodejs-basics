84a506e1c37cf8edbabf1d80afacb4fea13fc173
Viktor <velenic@mail.ru>
2022-06-02 17:52:05 +0300

    feat: add create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,14 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const create = async () => {
-    // Write your code here 
+  fs.writeFile(path.join(__dirname, 'files', 'fresh.txt'), 'I am fresh and young', (err) => {
+    if (err) throw err;
+    console.log('Файл создан');
+  })
 };
