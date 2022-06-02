664cf37eae8ec7f486f5f4a4866f0c54904f66d5
Viktor <velenic@mail.ru>
2022-06-02 17:55:33 +0300

    feat: add copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,21 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const copy = async () => {
-    // Write your code here 
+  fs.rm(path.join(__dirname, 'files-copy'), { recursive: true, force:true}, (err) => {
+    if (err) throw err;
+    fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
+        if (err) throw err;});
+        fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+            files.forEach(el => {
+            fs.appendFile(path.join(__dirname, `files-copy/${el}`),'',(err) => {if (err) throw err;});
+            fs.copyFile(path.join(__dirname, 'files', `${el}`), path.join(__dirname, 'files-copy', `${el}`), (err) => {if (err) throw err;});
+        });
+    });
+  });
 };
