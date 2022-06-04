25b3001e20d0fd44421ebc12e1a5b6176596ac8b
Viktor <velenic@mail.ru>
2022-06-04 21:18:40 +0300

    feat: add writable function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,3 +1,23 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import rl from 'readline';
+
+const readline = rl.createInterface({
+  input: process.stdin
+});
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const write = async () => {
-    // Write your code here 
+  console.log('Enter your text: ');
+  readline.on('line', (input) => {
+    fs.appendFile(path.join(__dirname, 'files', 'fileToWrite.txt'), `${input}\n`, ()=>{
+      console.log(`Your enter: ${input}`);
+    })
+  })
 };
+
+write()
