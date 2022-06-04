f6a50c12028bbea46fac53960e10d5504563a70e
Viktor <velenic@mail.ru>
2022-06-04 18:40:41 +0300

    feat: add env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
+import { env } from 'node:process';
+
 export const parseEnv = () => {
-    // Write your code here 
+  const rss = {
+    RSS_name1: process.env.RSS_name1,
+    RSS_name2: process.env.RSS_name2,
+  };
+  console.log(`RSS_name1=${rss.RSS_name1}; RSS_name2=${rss.RSS_name2}`);
 };
+
+parseEnv()
