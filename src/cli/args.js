7cbcb3bb4c81779b3593cdb4ec92aaf711e6fb27
Viktor <velenic@mail.ru>
2022-06-04 19:24:15 +0300

    feat: add args function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,9 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const flagIndex = process.argv.indexOf('--propName');
+  if (flagIndex !== -1) {
+    const message = process.argv.slice(2);
+    console.log(`propName is ${message[flagIndex - 1]}, prop2Name is ${message[flagIndex + 1]}`);
+  }
 };
+
+parseArgs()
