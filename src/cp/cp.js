e3afd7b22fd7533f369ee49b08009a598b4bfdc9
Viktor <velenic@mail.ru>
2022-06-05 21:57:22 +0300

    feat: add child process function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,23 @@
+import { spawn  } from 'child_process';
+
 export const spawnChildProcess = async (args) => {
-  process.stdin.pipe(args.stdin)
+  const child = spawn('find', args);
+
+  child.stdout.on('data', data => {
+    console.log(`stdout:\n${data}`);
+  });
+
+  child.stderr.on('data', data => {
+    console.error(`stderr: ${data}`);
+  });
+
+  child.on('error', (error) => {
+    console.error(`error: ${error.message}`);
+  });
+  
+  child.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
 };
+
+spawnChildProcess()
